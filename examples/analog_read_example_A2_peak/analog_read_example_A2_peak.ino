f4f047c9aa1475c6f32466c9c5ec1651364d08ee
Daniel M. Lofaro <dan@danlofaro.com>
2022-08-19 15:43:59 -0400

    still not reading peak for adc1.... will try fft maybe?

---
+++ examples/analog_read_example_A2_peak/analog_read_example_A2_peak.ino
@@ -0,0 +1,37 @@
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+
+// GUItool: begin automatically generated code
+AudioInputAnalog         adc1(A2);           //xy=192,87
+AudioAmplifier           amp1;           //xy=443,96
+AudioAnalyzePeak         peak1;          //xy=613,104
+AudioConnection          patchCord1(adc1, amp1);
+AudioConnection          patchCord2(amp1, peak1);
+// GUItool: end automatically generated code
+
+
+void setup() {
+  // put your setup code here, to run once:
+  AudioMemory(16);
+  amp1.gain(1.0);
+  //pinMode(A2, INPUT);
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(peak1.available())
+  {
+    Serial.println(peak1.read());
+  }
+  else
+  {
+    //Serial.println('.');
+  }
+  //Serial.println(peak1.read());
+  delay(10);
+}
